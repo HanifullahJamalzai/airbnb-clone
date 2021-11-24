ca77e642617b93ab676af4aa0f2463b16183f393
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2021-11-24 08:15:34 +0430

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import airbnb_logo from "../images/airbnb-logo.png";
+
+export default function Navbar() {
+	return (
+		<nav>
+			<img src={airbnb_logo} alg="logo" width="100px" />
+		</nav>
+	);
+}
